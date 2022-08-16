37797d8ace8681389db158548da414fb2f48848d
Fernando Sproviero <fernandospr@gmail.com>
2022-08-16 20:12:18 -0300

    Adding game over screen

--- app/src/main/java/com/yourcompany/android/tictactoe/ui/screens/GameScreen.kt
+++ app/src/main/java/com/yourcompany/android/tictactoe/ui/screens/GameScreen.kt
@@ -1,13 +1,14 @@
 package com.yourcompany.android.tictactoe.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.yourcompany.android.tictactoe.domain.model.GameState
@@ -17,10 +18,61 @@ import com.yourcompany.android.tictactoe.viewmodel.TicTacToeViewModel
 fun GameScreen(viewModel: TicTacToeViewModel) {
   val state: GameState by viewModel.state.observeAsState(GameState.Uninitialized)
 
-  Board(
-    board = state.board,
-    onBucketClick = { position -> viewModel.play(state.playerTurn, position) }
-  )
+  if (state.isOver) {
+    GameOverScreen(
+      playerWon = state.playerWon,
+      onNewGameClick = { viewModel.newGame() }
+    )
+  } else {
+    OngoingGameScreen(
+      playerTurn = state.playerTurn,
+      board = state.board,
+      onBucketClick = { position -> viewModel.play(state.playerTurn, position) }
+    )
+  }
+}
+
+@Composable
+fun GameOverScreen(
+  playerWon: Int,
+  onNewGameClick: () -> Unit
+) {
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    if (playerWon > 0) {
+      Text(text = "Player $playerWon won!")
+    } else {
+      Text(text = "It's a tie!")
+    }
+    Button(
+      modifier = Modifier.fillMaxWidth(),
+      onClick = onNewGameClick
+    ) {
+      Text(text = "New game!")
+    }
+  }
+}
+
+@Composable
+fun OngoingGameScreen(
+  playerTurn: Int,
+  board: Array<Array<Int>>,
+  onBucketClick: (position: Pair<Int, Int>) -> Unit
+) {
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Text(text = "Player Turn: $playerTurn")
+    Board(
+      board = board,
+      onBucketClick = { position -> onBucketClick(position) }
+    )
+  }
 }
 
 @Composable
